6d54f0da852a4cf058641f6e77913fed732de8d1
Ryan Farley <ryanfarley@airspacetechnologies.com>
2016-12-29 11:00:13 -0800

    Initial commit

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
@@ -0,0 +1,251 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.bluetoothlegatt;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Activity for scanning and displaying available Bluetooth LE devices.
+ */
+public class DeviceScanActivity extends Activity {
+
+    private class DeviceContainer implements Comparable<DeviceContainer> {
+        private BluetoothDevice device;
+        private int rssi;
+
+        public DeviceContainer(BluetoothDevice device, int rssi) {
+            this.device = device;
+            this.rssi = rssi;
+        }
+
+        @Override
+        public int compareTo(DeviceContainer deviceContainer) {
+            return Double.compare(deviceContainer.rssi, this.rssi);
+        }
+    }
+
+    private ArrayList<DeviceContainer> mDeviceContainers;
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mDisplayInitialMessage = true;
+    private boolean mScanningComplete = false;
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 2000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_air_trace);
+
+        mDeviceContainers = new ArrayList<>();
+
+        // Use this check to determine whether BLE is supported on the device.  Then you can
+        // selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                MY_PERMISSIONS_REQUEST_LOCATION);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(R.string.looking_for_airtrace);
+
+        scanLeDevice(true);
+
+        // Show the scanning message to the user for 5 seconds
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        mDisplayInitialMessage = false;
+                    }
+                },
+                5000
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    scanLeDevice(true);
+
+                    // Show the scanning message to the user for 5 seconds
+                    new java.util.Timer().schedule(
+                            new java.util.TimerTask() {
+                                @Override
+                                public void run() {
+                                    mDisplayInitialMessage = false;
+                                }
+                            },
+                            5000
+                    );
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(R.string.looking_for_airtrace);
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            new java.util.Timer().schedule(
+                    new java.util.TimerTask() {
+                        @Override
+                        public void run() {
+                            stopScan();
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    mScanningComplete = true;
+                                    updateUI();
+                                    // send result to server
+                                    // on callback take them to the next screen
+                                }
+                            });
+                        }
+                    },
+                    30000
+            );
+            startScan();
+        } else {
+            stopScan();
+        }
+        invalidateOptionsMenu();
+    }
+
+    private void startScan() {
+        mBluetoothAdapter.startLeScan(mLeScanCallback);
+    }
+
+    private void stopScan() {
+        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+    }
+
+    private void addDeviceContainer(BluetoothDevice device, int rssi) {
+        for (DeviceContainer container : mDeviceContainers) {
+            if (container.device.getName().equals(device.getName())) {
+                container.rssi = rssi;
+                Collections.sort(mDeviceContainers);
+                updateUI();
+                return;
+            }
+        }
+        mDeviceContainers.add(new DeviceContainer(device, rssi));
+        Collections.sort(mDeviceContainers);
+        updateUI();
+    }
+
+    private void updateUI() {
+        if (mScanningComplete) {
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(R.string.airtrace_found);
+        } else if (!mDisplayInitialMessage) {
+            DeviceContainer closestDevice = mDeviceContainers.get(0);
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText("Searching..." + closestDevice.device.getName() + " " + String.valueOf(closestDevice.rssi));
+        }
+    }
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi, byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    final String deviceName = device.getName();
+                    if (deviceName != null && deviceName.startsWith("LUGLOC")) {
+                        Log.v("LUGLOC", deviceName);
+                        addDeviceContainer(device, rssi);
+                    }
+                }
+            });
+        }
+    };
+}
